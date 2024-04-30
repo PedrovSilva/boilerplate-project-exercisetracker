0b539134e944e1e0b9754a5961aa47f745da9b1a
Pedro Silva <pedrovitordcruzsilva@gmail.com>
2024-04-30 00:49:19 +0000

    feat: add newUser

--- index.js
+++ index.js
@@ -1,16 +1,41 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongo = require('moongose')
 require('dotenv').config()
 
+mongo.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+const userSchema = new mongo.Schema({
+  _id:{
+    type: mongoose.Schema.Types.ObjectId,
+    auto:true
+  },
+  name:{
+    type: String,
+    require:true
+  }
+})
 
+const User = mongo.Model("User", userSchema)
 
+app.route("/api/users").post((req, res, done) => {
+  const newUser = req.body
+
+  newUser.Save((err, data) => {
+    if (err){
+      return done(err)
+    }
+    res.json({_id: data._id, 
+              username: data.name})
+    done(null, data)
+  })
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
